f32aacf0af0392b32cc6b4936c70420c8c174f9d
sree212212 <sreehitharayaly@gmail.com>
2025-06-16 18:32:44 +0530

    commit

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,62 +1,58 @@
 import { Webhook } from "svix";
 import User from "../models/User.js";
 
-//api controller function to manage clerk user with database
+export const clerkWebhooks = async (req, res) => {
+  try {
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-export const clerkWebhooks=async (req, res) => {
-try {
-    //Create a svix instance with clerk webhook secrret
-    const whook=new Webhook(process.env.CLERK_WEBHOOK_SECRET);
-    //verify headres
-    await whook.verify(JSON.stringify(req.body),{
-     "svix-id":req.headers["svix-id"],
-      "svix-timestamp":req.headers["svix-timestamp"], 
-      "svix-signature":req.headers["svix-signature"]
-    })
+    const payload = req.body.toString(); // convert Buffer to string
 
-    //getting data from request body,data payload of user and type is like update delete etc
-    const {data, type} = req.body;
+    const headers = {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"]
+    };
 
-    //switch case for differnt types of webhook events
-    switch(type){
-        case 'user.created':{
-          const userData={
-            _id:data.id,
-            email:data.email_addresses[0].email_address,
-            name:data.first_name + " " + data.last_name,
-            image:data.image_url,
-            resume:''
-          }
-          //save in database
-          await User.create(userData);
-            res.json({});
-            break;
-        }
-        case 'user.updated':{
- const userData={
-           
-            email:data.email_addresses[0].email_address,
-            name:data.first_name + " " + data.last_name,
-            image:data.image_url,
-            
-          }
-          await User.findByIdAndUpdate(data.id, userData);
-          res.json({});
-            break;
-        }
-        case 'user.deleted':{
-await User.findByIdAndDelete(data.id);
-            res.json({});
-            break;
-        }
-        default:
-            break;
+    const evt = whook.verify(payload, headers);
+    const { data, type } = evt;
 
-        
+    switch (type) {
+      case 'user.created': {
+        const userData = {
+          _id: data.id,
+          email: data.email_addresses[0].email_address,
+          name: data.first_name + " " + data.last_name,
+          image: data.image_url,
+          resume: ''
+        };
+        await User.create(userData);
+        res.status(200).json({});
+        break;
+      }
+
+      case 'user.updated': {
+        const userData = {
+          email: data.email_addresses[0].email_address,
+          name: data.first_name + " " + data.last_name,
+          image: data.image_url,
+        };
+        await User.findByIdAndUpdate(data.id, userData);
+        res.status(200).json({});
+        break;
+      }
+
+      case 'user.deleted': {
+        await User.findByIdAndDelete(data.id);
+        res.status(200).json({});
+        break;
+      }
+
+      default:
+        res.status(200).json({});
+        break;
     }
-} catch (error) {
-    
-    console.log(error.message);
-    res.json({success:false, message:'webhooks error'});
-}
-}
+  } catch (error) {
+    console.error("Webhook Error:", error);
+    res.status(400).json({ success: false, message: 'Webhook error', error: error.message });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -1,33 +1,40 @@
-
-import 'dotenv/config'
 import express from 'express';
 import cors from 'cors';
 import connectDB from './config/db.js';
-import './config/instrument.js'
+import bodyParser from 'body-parser';
+import { clerkWebhooks } from './controllers/webhooks.js';
 import * as Sentry from "@sentry/node";
-import {clerkWebhooks} from './controllers/webhooks.js'
- 
-//initialise express app
+import './config/instrument.js'; // Assuming this sets up Sentry
+
+import 'dotenv/config';
+
+// Initialize Express app
 const app = express();
 
-//connect to database
+// Connect to MongoDB
 await connectDB();
-//middleware
-app.use(cors());
-app.use(express.json());
-//routes
-app.get('/', (req, res) => res.send("api working"))
 
-app.get("/debug-sentry.", function mainHandler(req, res) {
+// Raw body parser ONLY for Clerk Webhooks
+app.use('/webhooks', bodyParser.raw({ type: 'application/json' }));
+
+// General middleware
+app.use(cors());
+app.use(express.json()); // for non-webhook JSON requests
+
+// Routes
+app.get('/', (req, res) => res.send("API working"));
+
+app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
 
-app.post('/webhooks',clerkWebhooks);
+app.post('/webhooks', clerkWebhooks);
 
-//port on which server will run
-const PORT = process.env.PORT || 5000;
+// Sentry error handler
 Sentry.setupExpressErrorHandler(app);
 
+// Start server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
